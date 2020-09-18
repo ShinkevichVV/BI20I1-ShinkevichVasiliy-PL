3e211ff1cde737ac05ac1fbb7b4ed0f9eb467964
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-09-19 04:12:30 +0600

    Lab_2

---
+++ labSIB2/labSIB2/labSIB2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x;
+	srand(time(NULL));
+	int a = rand() % 100;
+	cout << a;
+	do {
+		cout << "Введите число:" << endl;
+		cin >> x;
+		if (a < x) {
+			cout << "Загаданное число меньше" << endl;
+		}
+		else if (a > x) {
+			cout << "Загаданное число больше" << endl;
+		}
+		else {
+			cout << "Число угадано:" << a << endl;
+			system("pause");
+			return 0;
+		}
+	} while (a != x);
+}
