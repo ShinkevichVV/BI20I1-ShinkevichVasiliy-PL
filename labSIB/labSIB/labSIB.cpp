aa8309002d004634fe3fc00cfe2e3aa20f002459
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-09-19 03:20:13 +0600

    Lab_1

---
+++ labSIB/labSIB/labSIB.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+		int x,s,d,v;
+		cout << "Введите число:" << endl;
+		cin >> x;
+		s = (x / 2) + 1;
+		d = x / 2;
+		v = x / 8;
+		cout << "Количество чётных чисел:" << s << endl;
+		cout << "Количество чисел, которые делятся на два без остатка:" << d << endl;
+		cout << "Количество чисел, которые делятся на восемь без остатка:" << v << endl;
+		return 0;
+}
