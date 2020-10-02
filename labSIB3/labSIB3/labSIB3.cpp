34e6bd99c6d64dc9dc5525cc3cdd2417444e38ba
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-10-03 03:20:23 +0600

    Add files via upload

---
+++ labSIB3/labSIB3/labSIB3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x, s, b, z;
+	s = 1000;
+	cout << "Баланс равен = " << s << endl;
+	do {
+		srand(time(NULL));
+		int a = rand() % 22;
+		cout << a;
+		do {
+			cout << "Введите сумму ставки: " << endl;
+			cin >> b;
+			if ((b == 0) || (b > s) || (b < 0)) {
+				cout << "Введите другую сумму " << endl;
+			}
+			else {
+				cout << "Ставка принята: " << b << endl;
+			}
+		} while ((b > s) || (b == 0));
+		do {
+			cout << "Введите число от 0 до 21: " << endl;
+			cin >> x;
+		} while ((x > 21) || (x < 0));
+		if (x == a) {
+			cout << "Вы угадали число: " << a << endl;
+			s += b * 4;
+			cout << "Баланс равен = " << s << endl;
+		}
+		else if ((x == a - 2) || (x == a + 2)) {
+			cout << "Число в пределе +-2: " << a << endl;
+			s += b * 2;
+			cout << "Баланс равен = " << s << endl;
+		}
+		else {
+			cout << "Вы проиграли " << a << endl;
+			s -= b;
+			cout << "Баланс равен = " << s << endl;	
+		}
+	} while ((s == 0) || (s < 3000));
+}
