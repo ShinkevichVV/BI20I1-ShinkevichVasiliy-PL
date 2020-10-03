2b0c36b75665d5d26de4090a1eeedc76402afa93
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-10-03 19:00:53 +0600

    Add files via upload

---
+++ lab3Podmena/lab3Podmena/lab3Podmena.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int i, j, x;
+	int f[5];
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int* pa;
+	pa = &a[0];
+	setlocale(LC_ALL, "Russian");
+	for (i = 0; i < 5; i++) {
+		cout << a[i] << endl;
+	}
+	for (i = 0; i < 5; i++) {
+		*pa = *(pa + 4);
+		*(pa + 1) = *(pa + 3);
+		*(pa + 3) = *(pa + 1);
+		*(pa + 4) = *pa;
+	}
+	cout << "Измененная" << endl;
+	for (i = 0; i < 5; i++) {
+		cout << a[i] << endl;
+	}
+}
