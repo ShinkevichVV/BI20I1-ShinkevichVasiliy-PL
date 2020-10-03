78ea1c404f4c12f443824e238dfacb61dde996b6
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-10-03 17:51:40 +0600

    Redone

--- labSIB/labSIB/labSIB.cpp
+++ labSIB/labSIB/labSIB.cpp
@@ -3,14 +3,25 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-		int x,s,d,v;
-		cout << "Введите число:" << endl;
-		cin >> x;
-		s = (x / 2) + 1;
-		d = x / 2;
-		v = x / 8;
+	int x,m, s, d, v;
+	int count=x=m=s=d=v=0;
+	cout << "Введите число:" << endl;
+	cin >> x;
+	while (x >= count) {
+		cout << count << endl;
+		if (count % 2 == 0) {
+			s++;
+		}
+		if (count % 2 == 0) {
+			d++;
+		}
+		if (count % 8 == 0) {
+			v++;
+		}
+		count++;
+	}
 		cout << "Количество чётных чисел:" << s << endl;
 		cout << "Количество чисел, которые делятся на два без остатка:" << d << endl;
 		cout << "Количество чисел, которые делятся на восемь без остатка:" << v << endl;
 		return 0;
-}
+	}
