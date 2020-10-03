cc1f28615ee7a01fb0baf38bae1b43940debbcc7
ShinkevichVV <71521590+ShinkevichVV@users.noreply.github.com>
2020-10-03 17:46:32 +0600

    Sortirovka Puzirkami

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int j, i, m[100];
+	srand(time(NULL));
+	setlocale(LC_ALL, "Russian");
+	for (i = 0; i < 100; i++) {
+		m[i] = rand() % 500;
+		cout << m[i] << endl;
+	}
+	for (i = 0; i < 100; i++) {
+		for (j = 0; j < 100 - (i+1); j++) {
+			if (m[j] > m[j + 1]) {
+				int t = m[j];
+				m[j] = m[j + 1];
+				m[j + 1] = t;
+			}
+		}
+	} cout << "Сортировка:" << endl;
+	for (i = 0; i < 100; i++) {
+		cout << i << " Элемент = " << m[i] << " " << endl;
+	}
+	system("pause");
+	return 0;
+
+}
+	
+
+	
